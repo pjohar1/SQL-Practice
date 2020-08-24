7f4a3876e444d62c84d55f93c63d07e317629b3f
Punardeep Singh Johar <67790843+pjohar1@users.noreply.github.com>
2020-08-23 23:13:13 -0400

    Add files via upload

--- Revising Select Query.sql
+++ Revising Select Query.sql
@@ -1,4 +1,3 @@
-Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
 
 The CITY table is described as follows:
 
@@ -12,10 +11,41 @@ DISTRICT	VARCHAR2(20)
 POPULATION	NUMBER
 
 
-
-
-
+Problem 1:
+Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
 
 SQL Query:
 
 SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA'
+
+Problem 2:
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+
+SQL Query:
+
+SELECT DISTINCT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA'
+
+Problem 3:
+Query all columns (attributes) for every row in the CITY table.
+
+SQL Query:
+SELECT * FROM CITY
+
+Problem 4:
+Query all columns for a city in CITY with the ID 1661.
+
+SQL Query:
+SELECT * FROM CITY WHERE ID = 1661;
+
+Problem 5:
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+SQL Query:
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN';
+
+Problem 6:
+Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+SQL Query:
+SELECT DISTINCT NAME FROM CITY WHERE COUNTRYCODE = 'JPN';
+
