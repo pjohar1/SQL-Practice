aec100954f67a2886387e7b7e298c2eb1a88665d
Punardeep Singh Johar <67790843+pjohar1@users.noreply.github.com>
2020-08-24 16:25:43 -0400

    Add files via upload

--- Weather Observation.sql
+++ Weather Observation.sql
@@ -97,20 +97,82 @@ AND LOWER(SUBSTR(CITY,1,1)) NOT IN ('a', 'e', 'i', 'o', 'u');
 
 
 Problem 12:
+Query the following two values from the STATION table:
+
+The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+The sum of all values in LONG_W rounded to a scale of 2 decimal places.
 
 SQL Query:
+SELECT ROUND(SUM(LAT_N),2) AS lat, ROUND(SUM(LONG_W),2) AS lon FROM STATION
 
 
 Problem 13:
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. Truncate your answer to 4 decimal places.
 
 SQL Query:
+SELECT TRUNCATE(SUM(LAT_N),4) FROM STATION WHERE LAT_N > 38.7880 AND LAT_N < 137.2345
 
 
 Problem 14:
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. Truncate your answer to 4 decimal places
 
 SQL Query:
+SELECT TRUNCATE(MAX(LAT_N),4) FROM STATION WHERE LAT_N < 137.2345
 
 
 Problem 15:
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to 4 decimal places.
 
 SQL Query:
+SELECT ROUND(LONG_W,4) FROM STATION WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345)
+
+
+Problem 16:
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7880. Round your answer to 4 decimal places.
+
+SQL Query:
+SELECT ROUND(MIN(LAT_N),4) FROM STATION WHERE LAT_N > 38.7780
+
+
+Problem 17:
+Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7880. Round your answer to 4 decimal places.
+
+SQL Query:
+SELECT ROUND(LONG_W,4) FROM STATION WHERE LAT_N = (SELECT MIN(LAT_N) FROM STATION WHERE LAT_N > 38.7780)
+
+
+Problem 18:
+Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
+
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places.
+
+SQL Query:
+SELECT ROUND(ABS(MIN(LAT_N) - MAX(LAT_N)) + ABS(MIN(LONG_W) - MAX(LONG_W)),4) FROM STATION
+
+Problem 19:
+Consider P1(a,b) and P2(c,d) to be two points on a 2D plane where (a,b) are the respective minimum and maximum values of Northern Latitude (LAT_N) and (c,d) 
+are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+Query the Euclidean Distance between points p1 and p2 and format your answer to display 4 decimal digits.
+
+SQL Query:
+SELECT TRUNCATE(SQRT( POWER(MIN(LAT_N)- MAX(LAT_N),2) + POWER(MIN(LONG_W)- MAX(LONG_W),2)),4) FROM STATION
+
+
+Problem 20:
+A median is defined as a number separating the higher half of a data set from the lower half. 
+Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  decimal places.
+
+SQL Query:
+WITH cte AS (SELECT s.*, ROW_NUMBER() OVER (ORDER BY LAT_N) rn,
+            COUNT(*) OVER () cnt
+            FROM STATION s)
+
+SELECT ROUND(AVG(LAT_N),4) FROM cte 
+WHERE 
+(MOD(cnt, 2) = 0 AND rn IN (FLOOR(cnt/2), FLOOR(cnt/2)+1))
+OR
+(MOD(cnt,2) = 1) AND rn IN (FLOOR(cnt/2)+1)
